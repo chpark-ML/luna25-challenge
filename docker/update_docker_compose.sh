bcce44b50bf50a7e3df7c23f37beae3568a406fc
Changhyun Park <41136459+chpark-ML@users.noreply.github.com>
2025-03-13 18:22:53 +0900

    Update build context (#4)
    * refactoring
    
    * updates
    
    * updates
    
    * remove not used files
    
    * hotfix, separator

--- docker/update_docker_compose.sh
+++ docker/update_docker_compose.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-NEW_SERVICE_NAME="$1"
-DOCKER_COMPOSE_TEMPLATE_PATH="$2"
-DOCKER_COMPOSE_PATH="$3"
+SERVICE_NAME_BASE="$1"
+SERVICE_NAME_RESEARCH="$2"
 
-# docker-compose 파일에서 SERVICE_NAME을 업데이트하여 임시 파일에 저장
-sed "s/\${SERVICE_NAME}/${NEW_SERVICE_NAME}/g" "${DOCKER_COMPOSE_TEMPLATE_PATH}" > "${DOCKER_COMPOSE_PATH}"
+DOCKER_COMPOSE_TEMPLATE_PATH="$3"
+DOCKER_COMPOSE_PATH="$4"
+
+# 첫 번째 sed 적용 후, 결과를 임시 파일에 저장
+sed "s/\${SERVICE_NAME_BASE}/${SERVICE_NAME_BASE}/g" "${DOCKER_COMPOSE_TEMPLATE_PATH}" > "${DOCKER_COMPOSE_PATH}.tmp"
+
+# 두 번째 sed를 임시 파일에 적용하여 최종 결과를 생성
+sed "s/\${SERVICE_NAME_RESEARCH}/${SERVICE_NAME_RESEARCH}/g" "${DOCKER_COMPOSE_PATH}.tmp" > "${DOCKER_COMPOSE_PATH}"
+
+# 임시 파일 삭제
+rm "${DOCKER_COMPOSE_PATH}.tmp"
 
 echo "Saved updated docker-compose file to ${DOCKER_COMPOSE_PATH}."
