3fb45a116f18350850d6d8c8e63233c16ba744b3
juho.jung <juho.jung@vuno.co>
2025-03-14 18:22:49 +0900

    resampling and save h5 file

---
+++ projects/resample/resampling.py
@@ -0,0 +1,56 @@
+import os
+
+import h5py
+import numpy as np
+import pydicom
+import SimpleITK as sitk
+import torch
+from scipy.ndimage import zoom
+from tqdm import tqdm
+
+
+def load_mha(file_path):
+    image = sitk.ReadImage(file_path)
+    volume = sitk.GetArrayFromImage(image)  # z, y, x
+    spacing = image.GetSpacing()  # (x_spacing, y_spacing, z_spacing)
+    origin = image.GetOrigin()  # (x_origin, y_origin, z_origin)
+    direction = image.GetDirection()  # flatten 3x3 matrix
+    return volume, spacing, origin, direction
+
+
+def resample_image(volume, spacing, new_spacing=(1.0, 1.0, 1.0)):
+    vol_tensor = torch.tensor(volume, dtype=torch.float32).unsqueeze(0).unsqueeze(0)  # (1, 1, z, y, x)
+    scale_factors = torch.tensor(
+        [spacing[2] / new_spacing[2], spacing[1] / new_spacing[1], spacing[0] / new_spacing[0]]
+    )
+    size = [int(dim * scale) for dim, scale in zip(volume.shape, scale_factors)]
+    resampled = torch.nn.functional.interpolate(vol_tensor, size=size, mode="trilinear", align_corners=False)
+    return resampled.squeeze().numpy(), new_spacing
+
+
+def save_to_h5(output_path, volume, resampled, spacing):
+    with h5py.File(output_path, "w") as h5f:
+        h5f.create_dataset("volume_image", data=volume)
+        h5f.create_dataset("resampled_image", data=resampled)
+        h5f.create_dataset("spacing", data=spacing)
+
+
+def process_mha_to_h5(mha_dir, output_dir, new_spacing=(1.0, 1.0, 1.0)):
+    os.makedirs(output_dir, exist_ok=True)
+    for file in tqdm(os.listdir(mha_dir)):
+        if file.endswith(".mha"):
+            file_path = os.path.join(mha_dir, file)
+            volume, spacing, origin, direction = load_mha(file_path)
+            resampled, resampled_spacing = resample_image(volume, spacing, new_spacing)
+            output_path = os.path.join(output_dir, file.replace(".mha", ".h5"))
+            save_to_h5(output_path, volume, resampled, resampled_spacing)
+            print(f"Saved: {output_path}")
+
+
+if __name__ == "__main__":
+
+    dicom_dir = "/team/team_blu3/lung/data/2_public/LUNA25_Original/luna25_images"
+    output_dir = "/nvme1/5_personal/juhojung/LUNA25_resampled"
+    new_spacing = (1.0, 0.67, 0.67)
+
+    process_mha_to_h5(dicom_dir, output_dir, new_spacing)
