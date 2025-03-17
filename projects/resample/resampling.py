de3be7212e51a85266721d512bd2add205027486
Juho Jung/정주호 <78071411+JuHo-Jung@users.noreply.github.com>
2025-03-17 13:25:57 +0900

    Hotfix/fix pixel spacing (#15)
    * fix order of new_pixel_spacing
    
    * change save path, no space left in nvme

--- projects/resample/resampling.py
+++ projects/resample/resampling.py
@@ -21,7 +21,7 @@ def load_mha(file_path):
 def resample_image(volume, spacing, new_spacing=(1.0, 1.0, 1.0)):
     vol_tensor = torch.tensor(volume, dtype=torch.float32).unsqueeze(0).unsqueeze(0)  # (1, 1, z, y, x)
     scale_factors = torch.tensor(
-        [spacing[2] / new_spacing[2], spacing[1] / new_spacing[1], spacing[0] / new_spacing[0]]
+        [spacing[2] / new_spacing[0], spacing[1] / new_spacing[1], spacing[0] / new_spacing[2]]
     )
     size = [int(dim * scale) for dim, scale in zip(volume.shape, scale_factors)]
     resampled = torch.nn.functional.interpolate(vol_tensor, size=size, mode="trilinear", align_corners=False)
@@ -50,7 +50,7 @@ def process_mha_to_h5(mha_dir, output_dir, new_spacing=(1.0, 1.0, 1.0)):
 if __name__ == "__main__":
 
     dicom_dir = "/team/team_blu3/lung/data/2_public/LUNA25_Original/luna25_images"
-    output_dir = "/nvme1/5_personal/juhojung/LUNA25_resampled"
+    output_dir = "/team/team_blu3/lung/data/2_public/LUNA25_resampled"
     new_spacing = (1.0, 0.67, 0.67)
 
     process_mha_to_h5(dicom_dir, output_dir, new_spacing)
