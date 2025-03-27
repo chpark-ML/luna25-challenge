1b40c3ccb831ad6ae6b5bc1c0ab479ff6716a657
chpark-ML <bch0322@gmail.com>
2025-03-27 15:45:45 +0900

    add cv script

---
+++ projects/downstream/scripts/cross_validation/cross_validation_base.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#args
+gpu_num=$1
+val_fold=$2
+
+cd /opt/challenge/projects/downstream
+
+benchmark=True
+deterministic=False
+use_amp=True
+
+run_name=cv_val_fold${val_fold}
+
+HYDRA_FULL_ERROR=1 python3 main.py \
+  experiment_tool.run_name=${run_name} \
+  "inputs.dataset.dataset_infos.luna25.val_fold=[${val_fold}]" \
+  trainer.benchmark=${benchmark} \
+  trainer.deterministic=${deterministic} \
+  trainer.use_amp=${use_amp} \
+  trainer.gpus=${gpu_num} \
+  trainer.fast_dev_run=False
