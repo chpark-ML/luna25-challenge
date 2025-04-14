8e9f6476f57a46651c5fa8d08f3e2ce887bd3aa2
chpark-ML <bch0322@gmail.com>
2025-04-14 16:36:17 +0900

    update device info in processor

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,3 +1,3 @@
 #rsync -rP changhyun.park@172.31.10.41:~/luna25-challenge/data_lake/prepare_dataset/fig_volume/1 ~/Downloads/works
-file_name=luna25-baseline-3d-algorithm-open-development-phase_2025-04-12T11-15-22.328382871+09-00.tar.gz
+file_name=nodulex_2025-04-12T13-30-48.630416686+09-00.tar.gz
 rsync -rP changhyun.park@172.31.10.41:~/luna25-challenge/submission/${file_name} ~/Downloads/works
