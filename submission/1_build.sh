d4d43617aceee45915633bf0a00d5c3869c3a1ee
chpark-ML <bch0322@gmail.com>
2025-05-17 12:37:01 +0900

    refactoring processor path

--- submission/1_build.sh
+++ submission/1_build.sh
@@ -7,11 +7,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source "$SCRIPT_DIR/config.sh"
 
-# copy model weight
-echo "Copying weights"
-rm -rf "${SCRIPT_DIR}/resources"
-mkdir -p "${SCRIPT_DIR}/resources"
-cp -r  /team/team_blu3/lung/project/luna25/weights/${DOCKER_IMAGE_TAG}/* "${SCRIPT_DIR}/resources"
 
 echo "=+= (Re)build base image"
 docker --debug build \
@@ -21,6 +16,18 @@ docker --debug build \
   "${SCRIPT_DIR}" 2>&1
 
 echo "=+= (Re)build app image"
+
+
+echo "Copying weights"
+rm -rf "${SCRIPT_DIR}/resources"
+mkdir -p "${SCRIPT_DIR}/resources"
+
+# copy model weight
+cp -r  /team/team_blu3/lung/project/luna25/weights/${DOCKER_IMAGE_TAG}/* "${SCRIPT_DIR}/resources"
+
+# copy shared_lib
+cp -r "${SCRIPT_DIR}/../shared_lib" "${SCRIPT_DIR}/shared_lib"
+
 docker build \
   --tag "${DOCKER_IMAGE_TAG}" \
   -f "${SCRIPT_DIR}/Dockerfile_app" \
--- submission/input_processor.py
+++ submission/input_processor.py
@@ -1,11 +1,11 @@
 import logging
-from pathlib import Path
 from typing import Dict, List
 
-import dataloader
 import numpy as np
 import SimpleITK
-import torch
+
+
+from shared_lib.malignancy_processor import MalignancyProcessor
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -67,83 +67,7 @@ def itk_image_to_numpy_image(input_image):
     return numpyImage, header
 
 
-# define processor
-class MalignancyProcessor:
-    """
-    Loads a chest CT scan, and predicts the malignancy around a nodule
-    """
-
-    def __init__(self, config_models=None, mode="3D", device=torch.device("cuda:0"), suppress_logs=False):
-        self.device = device
-        self.size_px_xy = 72
-        self.size_px_z = 48
-        self.size_mm = 50
-        self.order = 1
-
-        self.suppress_logs = suppress_logs
-        if not self.suppress_logs:
-            logging.info("Initializing the deep learning system")
-
-        self.mode = mode
-        self.model_root = "/opt/app/resources/"
-
-        self.models = dict()
-        for model_indicator, config_model in config_models.items():
-            model_path = Path(config_model.root_path) / config_model.exp_name / config_model.file_name
-            self.models[model_indicator] = torch.jit.load(model_path, map_location=self.device).to(self.device).eval()
-
-    def _extract_patch(self, image, header, coord, output_shape, mode) -> np.array:
-        patch = dataloader.extract_patch(
-            CTData=image,
-            coord=coord,
-            srcVoxelOrigin=header["origin"],
-            srcWorldMatrix=header["transform"],
-            srcVoxelSpacing=header["spacing"],
-            output_shape=output_shape,
-            voxel_spacing=(
-                self.size_mm / self.size_px_z,
-                self.size_mm / self.size_px_xy,
-                self.size_mm / self.size_px_xy,
-            ),
-            coord_space_world=True,
-            mode=mode,
-            order=self.order,
-        )  # (1, w, h, d)
-
-        # ensure same datatype...
-        patch = patch.astype(np.float32)
-
-        # clip and scale...
-        patch = dataloader.clip_and_scale(patch)
-        return patch  # (1, w, h, d)
-
-    def _prepare_patch(self, image, header, coord, mode="3D") -> torch.Tensor:
-        if not self.suppress_logs:
-            logging.info("Processing in " + mode)
-
-        assert mode == "3D"
-        output_shape = [self.size_px_z, self.size_px_xy, self.size_px_xy]
-        patch = self._extract_patch(image, header, coord, output_shape, mode=mode)  # (1, w, h, d)
-        patch = torch.from_numpy(patch).to(self.device)
-
-        return patch.unsqueeze(0)  # 1, 1, w, h, d
-
-    def predict(self, numpy_image, header, coord):
-        patch = self._prepare_patch(numpy_image, header, coord, self.mode)
-
-        probs = list()
-        for model_name, model in self.models.items():
-            logits = model(patch)
-            logits = logits.data.cpu().numpy()
-            probs.append(torch.sigmoid(torch.from_numpy(logits)).numpy())
-
-        probs = np.stack(probs, axis=0)  # shape: (num_models, ...)
-        mean_probs = np.mean(probs, axis=0)
-
-        return mean_probs
-
-
-class ImageProcessor:
+class InputProcessor:
     def __init__(self, config_models, mode="3D"):
         """
         Parameters
