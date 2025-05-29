ab8f2735846f10b884655a3fe76b6fca0f0660d2
chpark-ML <bch0322@gmail.com>
2025-05-29 15:21:14 +0900

    add main inference script draft

---
+++ analyzer/nodule_attr/main_inference.py
@@ -0,0 +1,46 @@
+import logging
+from pathlib import Path
+
+import hydra
+from omegaconf import DictConfig
+
+from shared_lib.enums import RunMode
+from shared_lib.utils.utils import print_config, set_seed
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@hydra.main(version_base="1.2", config_path="configs", config_name="config_inference")
+def main(config: DictConfig):
+    # print config
+    print_config(config, resolve=True)
+
+    # set seed
+    set_seed()
+
+    # runner
+    models = dict()
+    for model_indicator, config_model in config.models.items():
+        models[model_indicator] = hydra.utils.instantiate(config_model)
+    processor = hydra.utils.instantiate(config.processor, models=models)
+
+    # run modes
+    run_modes = [RunMode(m) for m in config.run_modes] if "run_modes" in config else [x for x in RunMode]
+
+    # loader
+    loaders = {
+        mode: hydra.utils.instantiate(
+            config.loader,
+            dataset={"mode": mode},
+            drop_last=(mode == RunMode.TRAIN),
+            shuffle=(mode == RunMode.TRAIN),
+        )
+        for mode in run_modes
+    }
+
+    # inference and save nodule attributes and radiomic features
+
+
+if __name__ == "__main__":
+    main()
--- shared_lib/processor/nodule_attr_processor.py
+++ shared_lib/processor/nodule_attr_processor.py
@@ -64,53 +64,45 @@ class NoduleAttrProcessor(BaseProcessor):
 
         return ensemble_output
 
-    # def inference(self, loader, mode, sanity_check=False):
-    #     list_probs = list()
-    #     dict_probs = {model_name: [] for model_name in self.models.keys()}
-    #     list_annots = list()
-    #     list_annot_ids = list()
-    #
-    #     for data in tqdm(loader):
-    #         # prediction
-    #         patch_image = data["image"].to(self.device)
-    #
-    #         # annotation
-    #         annot = data["label"].to(self.device).float()
-    #         annot_ids = data["ID"]
-    #
-    #         # inference (model-wise)
-    #         batch_probs = list()
-    #         for model_name, model in self.models.items():
-    #             logits = model.get_prediction(patch_image)  # (B, 1)
-    #             print(logits)
-    #             prob = torch.sigmoid(logits)  # (B, 1)
-    #
-    #             # Save per-model probabilities
-    #             dict_probs[model_name].append(prob)
-    #
-    #             # Aggregate for overall averaging
-    #             batch_probs.append(prob)
-    #
-    #         # Mean across models
-    #         batch_probs = torch.stack(batch_probs)  # (num_models, B, 1)
-    #         mean_probs = torch.mean(batch_probs, dim=0)  # (B, 1)
-    #
-    #         list_probs.append(mean_probs)
-    #         list_annots.append(annot)
-    #         list_annot_ids.extend(annot_ids)
-    #         # sanity check
-    #         if sanity_check:
-    #             break
-    #
-    #     # Combine batches
-    #     probs = torch.vstack(list_probs)
-    #     annots = torch.vstack(list_annots)
-    #
-    #     # Convert to numpy
-    #     overall_probs = probs.squeeze().cpu().numpy()
-    #     overall_annots = annots.squeeze().cpu().numpy()
-    #
-    #     # Convert dict_probs to numpy
-    #     dict_probs = {k: torch.vstack(v).squeeze().cpu().numpy() for k, v in dict_probs.items()}
-    #
-    #     return overall_probs, overall_annots, list_annot_ids, dict_probs
+    def inference(self, loader, mode, sanity_check=False):
+        list_probs = list()
+        dict_probs = {model_name: [] for model_name in self.models.keys()}
+        list_annots = list()
+        list_annot_ids = list()
+
+        for data in tqdm(loader):
+            # prediction
+            patch_image = data["image"].to(self.device)
+
+            # annotation
+            annot = data["label"].to(self.device).float()
+            annot_ids = data["ID"]
+
+            # inference
+            batch_probs = list()
+            result = self.predict_given_patch(patch_image)
+            batch_probs.append(result)
+
+            # Mean across models
+            batch_probs = torch.stack(batch_probs)  # (num_models, B, 1)
+            mean_probs = torch.mean(batch_probs, dim=0)  # (B, 1)
+
+            list_probs.append(mean_probs)
+            list_annots.append(annot)
+            list_annot_ids.extend(annot_ids)
+            # sanity check
+            if sanity_check:
+                break
+
+        # Combine batches
+        probs = torch.vstack(list_probs)
+        annots = torch.vstack(list_annots)
+
+        # Convert to numpy
+        overall_probs = probs.squeeze().cpu().numpy()
+        overall_annots = annots.squeeze().cpu().numpy()
+
+        # Convert dict_probs to numpy
+        dict_probs = {k: torch.vstack(v).squeeze().cpu().numpy() for k, v in dict_probs.items()}
+
+        return overall_probs, overall_annots, list_annot_ids, dict_probs
