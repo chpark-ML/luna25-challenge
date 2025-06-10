a1202ad323bae8758b2159fde70ceb8fe4c10f48
chpark-vuno <changhyun.park@vuno.co>
2025-06-10 10:21:53 +0900

    CV exp using fmap40

--- trainer/nodule_attr/scripts/cross_validation/main.sh
+++ trainer/nodule_attr/scripts/cross_validation/main.sh
@@ -8,17 +8,13 @@ val_fold=${2:-0}
 model_num=5
 source /opt/challenge/trainer/common/model_config.sh ${model_num} ${val_fold}
 
-model_cfg=unet_3d_all
-do_segmentation=True
-
-model_name=cls_all_model_${model_num}_val_fold${val_fold}_seg${do_segmentation}
+model_name=cls_all_model_${model_num}_val_fold${val_fold}_6CV
 
 cd /opt/challenge/trainer/nodule_attr
 HYDRA_FULL_ERROR=1 python3 main.py \
   experiment_tool.experiment_name=lct-malignancy-attr \
   experiment_tool.run_group=baseline \
   experiment_tool.run_name=${model_name} \
-  model=${model_cfg} \
   model.model_repr.classifier.num_features=${num_features} \
   model.model_repr.classifier.use_gate=${use_gate} \
   model.model_repr.classifier.use_coord=${use_coord} \
@@ -26,6 +22,5 @@ HYDRA_FULL_ERROR=1 python3 main.py \
   criterion.aux_criterion.loss_weight=${aux_loss_weight} \
   criterion.entropy_criterion.loss_weight=${entropy_loss_weight} \
   "loader.dataset.dataset_info.pylidc.val_fold=[${val_fold}]" \
-  trainer.do_segmentation=${do_segmentation} \
   trainer.gpus=${gpu_num} \
   +debug=False
