ff75c54aa1bf3f4da3172fb22f2222983874e264
chpark-vuno <changhyun.park@vuno.co>
2025-06-13 17:05:14 +0900

    exp for fmap24

--- trainer/downstream/scripts/cross_validation_finetune/cross_validation_base.sh
+++ trainer/downstream/scripts/cross_validation_finetune/cross_validation_base.sh
@@ -6,10 +6,10 @@ val_fold=$2
 
 cd /opt/challenge/trainer/downstream
 
-run_name=cv_fine_val_fold${val_fold}_fmaps48
+run_name=cv_fine_val_fold${val_fold}_fmaps48_6CV
 
 nodule_attr_model_name=cls_all_model_5_val_fold${val_fold}_6CV
-model_path=/team/team_blu3/lung/project/luna25/pretrained/nodule_attr_seg_fmaps48/${nodule_attr_model_name}/model_loss.pth
+model_path=/team/team_blu3/lung/project/luna25/pretrained/nodule_attr_seg_fmaps24_6CV/${nodule_attr_model_name}/model_loss.pth
 
 HYDRA_FULL_ERROR=1 python3 main.py \
   experiment_tool.run_name=${run_name} \
