f0bcabd0f6d29dba83f010ff5d5d2201d4b947a0
chpark-vuno <changhyun.park@vuno.co>
2025-06-14 11:13:40 +0900

    add ablation study in finetuning

---
+++ trainer/downstream/scripts/ablation_study_finetune/main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# args
+gpu_num=${1:-0}
+param=${2:-0}
+val_fold=5
+
+run_name=lr_${param}_fine_tune
+
+nodule_attr_model_name=cls_all_model_5_val_fold${val_fold}_6CV
+model_path=/team/team_blu3/lung/project/luna25/pretrained/nodule_attr_seg_fmaps24_6CV/${nodule_attr_model_name}/model_loss.pth
+
+cd /opt/challenge/trainer/downstream
+HYDRA_FULL_ERROR=1 python3 main.py \
+  experiment_tool.experiment_name=lct-malignancy \
+  experiment_tool.run_group=baseline \
+  experiment_tool.run_name=${run_name} \
+  "loader.dataset.dataset_infos.luna25.val_fold=[${val_fold}]" \
+  scheduler.scheduler_repr.max_lr=${param} \
+  trainer.fine_tune_info.enable=True \
+  trainer.fine_tune_info.freeze_encoder=False \
+  trainer.fine_tune_info.pretrained_weight_path=${model_path} \
+  trainer.gpus=${gpu_num} \
+  +debug=False
