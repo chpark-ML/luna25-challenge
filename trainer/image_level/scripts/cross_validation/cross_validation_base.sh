fd748b8da9a1a6ecbde14723a26a1f8a0c2321e7
juho.jung <juho.jung@vuno.co>
2025-06-17 10:35:50 +0900

    mv path to nfs

--- trainer/image_level/scripts/cross_validation/cross_validation_base.sh
+++ trainer/image_level/scripts/cross_validation/cross_validation_base.sh
@@ -6,10 +6,11 @@ val_fold=$2
 
 cd /opt/challenge/trainer/image_level
 
-run_name=resnest_cv_val_fold${val_fold}_192
+unet_fmaps=8
+run_name=resnest_f_192_unet_f_${unet_fmaps}_lr_1e-4_scale_1.2_fold${val_fold}
 
 # Set path_patch_model based on val_fold
-path_patch_model="/team/team_blu3/lung/project/luna25/pretrained/downstream_single_scale_6CV/cv_fine_single_scale_val_fold${val_fold}_fmaps_24/model_auroc.pth"
+path_patch_model="/team/team_blu3/lung/project/luna25/pretrained/downstream_single_scale_6CV/cv_fine_single_scale_val_fold${val_fold}_fmaps_${unet_fmaps}/model_auroc.pth"
 
 HYDRA_FULL_ERROR=1 python3 main.py \
   experiment_tool.run_name=${run_name} \
