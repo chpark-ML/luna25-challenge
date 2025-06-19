bb0d2a0ad1a72a5df17ec9c8fc385dc033ee2c1d
chpark-vuno <changhyun.park@vuno.co>
2025-06-19 22:35:37 +0900

    add ms_classifier_v2

--- trainer/multi_modal/scripts/cross_validation/run.sh
+++ trainer/multi_modal/scripts/cross_validation/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /opt/challenge/trainer/nodule_attr/scripts/cross_validation_7fold || exit 1
+cd /opt/challenge/trainer/multi_modal/scripts/cross_validation || exit 1
 
 # tmux_window gpu_num val_fold
 paired_values=(
@@ -10,10 +10,9 @@ paired_values=(
   "4 3 3"
   "5 4 4"
   "6 5 5"
-  "7 6 6"
 )
 
-my_session=2
+my_session=0
 tmux new-session -d -s ${my_session}  # 새로운 tmux 세션 생성
 
 for pair in "${paired_values[@]}"
@@ -23,5 +22,5 @@ do
   val_fold=$(echo "$pair" | cut -d ' ' -f3)
 
   tmux new-window -t "${my_session}:" -n "${my_window}"  # 새로운 윈도우 생성
-  tmux send-keys -t "${my_session}:${my_window}" "bash main.sh ${gpu_num} ${val_fold}" Enter  # 해당 윈도우로 명령어 전달
+  tmux send-keys -t "${my_session}:${my_window}" "bash cross_validation_base.sh ${gpu_num} ${val_fold}" Enter  # 해당 윈도우로 명령어 전달
 done
