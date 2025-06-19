1e47ee8df90f1cdc5f1c9ff7e4d00b5340e14cc7
juho.jung <juho.jung@vuno.co>
2025-06-19 14:06:53 +0900

    change arg name

--- trainer/image_level/scripts/cross_validation/cross_validation_base.sh
+++ trainer/image_level/scripts/cross_validation/cross_validation_base.sh
@@ -7,7 +7,7 @@ val_fold=$2
 cd /opt/challenge/trainer/image_level
 
 unet_fmaps=24
-run_name=resnest_logit_level_zero_conv_f_192_unet_f_${unet_fmaps}_lr_1e-4_scale_1.5_fold${val_fold}
+run_name=resnest_logit_level_classifier_f_192_unet_f_${unet_fmaps}_lr_1e-4_scale_1.5_fold${val_fold}
 
 # Set path_patch_model based on val_fold
 path_patch_model="/team/team_blu3/lung/project/luna25/pretrained/downstream_single_scale_6CV/cv_fine_single_scale_val_fold${val_fold}_fmaps_${unet_fmaps}/model_auroc.pth"
@@ -19,4 +19,4 @@ HYDRA_FULL_ERROR=1 python3 main.py \
   trainer.gpus=${gpu_num} \
   trainer.fast_dev_run=False \
   model.model_repr.path_patch_model=${path_patch_model} \
-  model.model_repr.use_zero_conv=True
+  model.model_repr.use_zero_conv_classifier=False
