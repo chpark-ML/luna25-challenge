67390b6928e9cc2a573cc7c4811b314977dee8da
chpark-vuno <changhyun.park@vuno.co>
2025-06-19 19:31:52 +0900

    copy from downstream proj

---
+++ trainer/multi_modal/scripts/baseline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# args
+gpu_num=${1:-0}
+
+run_name=baseline_default
+
+cd /opt/challenge/trainer/multi_modal
+HYDRA_FULL_ERROR=1 python3 main.py \
+  experiment_tool.experiment_name=lct-malignancy \
+  experiment_tool.run_group=baseline \
+  experiment_tool.run_name=${run_name} \
+  trainer.gpus=${gpu_num}
