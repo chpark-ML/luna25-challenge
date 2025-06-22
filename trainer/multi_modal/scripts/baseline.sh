7299da312efbfda38c13be76c42a88166847e20b
chpark-vuno <changhyun.park@vuno.co>
2025-06-22 10:50:41 +0900

    updates

--- trainer/multi_modal/scripts/baseline.sh
+++ trainer/multi_modal/scripts/baseline.sh
@@ -3,11 +3,11 @@
 # args
 gpu_num=${1:-0}
 
-run_name=baseline_default
+run_name=baseline
 
 cd /opt/challenge/trainer/multi_modal
 HYDRA_FULL_ERROR=1 python3 main.py \
   experiment_tool.experiment_name=lct-malignancy-mm \
-  experiment_tool.run_group=baseline \
+  experiment_tool.run_group=default \
   experiment_tool.run_name=${run_name} \
   trainer.gpus=${gpu_num}
