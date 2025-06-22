9d84dbefebe716948ad7a9f213a28f0209d6f619
chpark-vuno <changhyun.park@vuno.co>
2025-06-22 10:50:41 +0900

    updates

--- trainer/multi_modal/scripts/debug.sh
+++ trainer/multi_modal/scripts/debug.sh
@@ -9,7 +9,7 @@ run_name=baseline_debug
 cd /opt/challenge/trainer/multi_modal
 HYDRA_FULL_ERROR=1 python3 main.py \
   experiment_tool.experiment_name=lct-malignancy-mm \
-  experiment_tool.run_group=baseline \
+  experiment_tool.run_group=debug \
   experiment_tool.run_name=${run_name} \
   loader.batch_size=${batch_size} \
   trainer.gpus=${gpu_num} \
