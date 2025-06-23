7f3871e181edddfd3f85c64cd489a4eacece0bf5
chpark-vuno <changhyun.park@vuno.co>
2025-06-23 12:16:02 +0900

    increase learning epoch for from scratch learning script only

--- trainer/downstream/scripts/cross_validation/cross_validation_base.sh
+++ trainer/downstream/scripts/cross_validation/cross_validation_base.sh
@@ -9,7 +9,7 @@ cd /opt/challenge/trainer/downstream
 run_name=cv_val_fold${val_fold}_6CV
 
 LR=1e-3
-epoch=20
+epoch=50
 
 HYDRA_FULL_ERROR=1 python3 main.py \
   experiment_tool.run_name=${run_name} \
