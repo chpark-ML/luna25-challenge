3df60197a1982c38997b5f287e7e06208c2fc00e
chpark-vuno <changhyun.park@vuno.co>
2025-06-23 12:14:05 +0900

    experiment settings

--- trainer/downstream/scripts/cross_validation_finetune_10CV/cross_validation_base.sh
+++ trainer/downstream/scripts/cross_validation_finetune_10CV/cross_validation_base.sh
@@ -6,14 +6,20 @@ val_fold=$2
 
 cd /opt/challenge/trainer/downstream
 
-run_name=cv_fine_val_fold${val_fold}_fmaps24_6CV
+run_name=cv_fine_val_fold${val_fold}_10CV
 
-nodule_attr_model_name=cls_all_model_5_val_fold${val_fold}_6CV
-model_path=/team/team_blu3/lung/project/luna25/pretrained/nodule_attr_seg_fmaps24_6CV_v2/${nodule_attr_model_name}/model_loss.pth
+LR=1e-3
+epoch=20
+fold_key=fold_10
+model_path=/team/team_blu3/lung/project/luna25/pretrained/nodule_attr_seg_KD_7CV/cls_all_KD_val_fold6/model_loss.pth
 
 HYDRA_FULL_ERROR=1 python3 main.py \
   experiment_tool.run_name=${run_name} \
   "loader.dataset.dataset_infos.luna25.val_fold=[${val_fold}]" \
+  "loader.dataset.dataset_infos.luna25.test_fold=[]" \
+  loader.dataset.dataset_infos.luna25.fold_key=${fold_key} \
+  scheduler.scheduler_repr.max_lr=${LR} \
+  trainer.max_epoch=${epoch} \
   trainer.fine_tune_info.enable=True \
   trainer.fine_tune_info.freeze_encoder=False \
   trainer.fine_tune_info.pretrained_weight_path=${model_path} \
