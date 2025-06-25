17b143ca8145a73afaf295855d8620e474467147
chpark-vuno <changhyun.park@vuno.co>
2025-06-25 20:53:28 +0900

    update run script

--- trainer/downstream/scripts/cross_validation_finetune_10CV_phase2/run.sh
+++ trainer/downstream/scripts/cross_validation_finetune_10CV_phase2/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /opt/challenge/trainer/downstream/scripts/cross_validation_finetune_6CV || exit 1
+cd /opt/challenge/trainer/downstream/scripts/cross_validation_finetune_10CV || exit 1
 
 # tmux_window gpu_num val_fold
 paired_values=(
@@ -10,9 +10,13 @@ paired_values=(
   "4 3 3"
   "5 4 4"
   "6 5 5"
+  "7 0 6"
+  "8 1 7"
+  "9 2 8"
+  "10 3 9"
 )
 
-my_session=fine_6cv
+my_session=fine_10cv
 tmux new-session -d -s ${my_session}  # 새로운 tmux 세션 생성
 
 for pair in "${paired_values[@]}"
---
+++ trainer/downstream/scripts/cross_validation_finetune_7CV_phase2/cross_validation_base.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#args
+gpu_num=$1
+val_fold=$2
+
+cd /opt/challenge/trainer/downstream
+
+run_name=cv_fine_val_fold${val_fold}_7CV_phase2
+
+LR=3e-4
+epoch=50
+model_path=/opt/challenge/trainer/downstream/outputs/default/cv_fine_val_fold${val_fold}_7CV/model_auroc.pth
+
+fold_key=fold
+all_folds=(0 1 2 3 4 5 6)
+all_fold_str=$(printf ",%s" "${all_folds[@]}")
+all_fold_str="[${all_fold_str:1}]"  # 앞의 쉼표 제거
+
+HYDRA_FULL_ERROR=1 python3 main.py \
+  experiment_tool.run_name=${run_name} \
+  loader=default \
+  "loader.dataset.dataset_infos.luna25.total_fold=${all_fold_str}" \
+  "loader.dataset.dataset_infos.luna25.val_fold=[${val_fold}]" \
+  "loader.dataset.dataset_infos.luna25.test_fold=[]" \
+  "loader.dataset.dataset_infos.luna25.fold_key=${fold_key}" \
+  scheduler.scheduler_repr.max_lr=${LR} \
+  trainer.max_epoch=${epoch} \
+  trainer.fine_tune_info.enable=True \
+  trainer.fine_tune_info.freeze_encoder=False \
+  trainer.fine_tune_info.pretrained_weight_path=${model_path} \
+  trainer.gpus=${gpu_num} \
+  trainer.fast_dev_run=False
