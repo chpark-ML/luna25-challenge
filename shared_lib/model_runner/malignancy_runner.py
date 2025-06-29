879fbdf3741cb31649a832a24053aabd57428842
chpark-vuno <changhyun.park@vuno.co>
2025-06-29 14:05:26 +0900

    make model return gate results

--- shared_lib/model_runner/malignancy_runner.py
+++ shared_lib/model_runner/malignancy_runner.py
@@ -4,7 +4,7 @@ from typing import Union
 import torch
 
 from shared_lib.model_runner.base_runner import ModelBaseTorchscript, ModelBaseTorchCheckpoint
-
+from trainer.common.constants import GATE_KEY, GATED_LOGIT_KEY, LOGIT_KEY
 
 class MalignancyRunner(ModelBaseTorchscript):
     def __init__(self, root_path, exp_name, file_name, device: Union[str, torch.device] = None):
@@ -29,8 +29,8 @@ class MalignancyRunner(ModelBaseTorchscript):
 class MalignancyRunnerCheckpoint(ModelBaseTorchCheckpoint):
     def __init__(self, root_path, exp_name, file_name, device: Union[str, torch.device] = None):
         checkpoint_path = Path(root_path) / exp_name / file_name
-        model_config_path = Path(root_path) / exp_name / ".hydra/config.yaml"
-        super().__init__(model_config_path=model_config_path, checkpoint_path=checkpoint_path, device=device)
+        config_path = Path(root_path) / exp_name / ".hydra/config.yaml"
+        super().__init__(config_path=config_path, checkpoint_path=checkpoint_path, device=device)
 
     @torch.no_grad()
     def get_prediction(self, input_tensor) -> torch.Tensor:
@@ -41,5 +41,16 @@ class MalignancyRunnerCheckpoint(ModelBaseTorchCheckpoint):
         Returns:
             torch.Tensor: Model output.
         """
-        breakpoint()
-        raise NotImplementedError
+
+        input_tensor = input_tensor.to(self.device)
+        output = self.model(input_tensor)
+
+        # output[LOGIT_KEY][self.model.classifier.target_attr_downstream]  # (B, 1)
+        # output[GATE_KEY][0]  # (B, 1, 24, 36, 36)
+        # output[GATE_KEY][1]  # (B, 1, 12, 18, 18)
+        # output[GATE_KEY][2]  # (B, 1, 6, 9, 9)
+        # output[GATED_LOGIT_KEY][self.model.classifier.target_attr_downstream][0]  # (B, 1, 24, 36, 36)
+        # output[GATED_LOGIT_KEY][self.model.classifier.target_attr_downstream][1]  # (B, 1, 12, 18, 18)
+        # output[GATED_LOGIT_KEY][self.model.classifier.target_attr_downstream][2]  # (B, 1, 6, 9, 9)
+
+        return output
