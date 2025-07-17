53de5a531e10b5961f8c1ef939b3133491568e0d
chpark-vuno <changhyun.park@vuno.co>
2025-07-17 17:04:08 +0900

    update vis

--- scripts/rsync_docker.sh
+++ scripts/rsync_docker.sh
@@ -1,3 +1,3 @@
 # copy from server to local
-file_name=nodulex-v4.0.1_2025-06-24T17-33-33.751413622+09-00.tar.gz
+file_name=nodulex-v5.0.8_2025-07-17T16-36-52.145076915+09-00.tar.gz
 rsync -rP changhyun.park@172.31.10.41:~/luna25-challenge/submission/${file_name} ~/Downloads/works
--- scripts/rsync_results.sh
+++ scripts/rsync_results.sh
@@ -1,6 +1,2 @@
 # copy from server to local
 rsync -rP changhyun.park@172.31.10.41:~/luna25-challenge/analyzer/malignancy/outputs/visualization_result/fig_volume ~/Downloads/works
-
-rsync -rP changhyun.park@172.31.10.41:~/luna25-challenge/trainer/downstream/datasets/test.png ~/Downloads/works
-
-rsync -rP changhyun.park@172.31.10.41:~/luna25-challenge/trainer/nodule_attr/datasets/test.png ~/Downloads/works
