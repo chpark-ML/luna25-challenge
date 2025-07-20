4189fed5e758282e968386789ab09a54f60ab32d
chpark-vuno <changhyun.park@vuno.co>
2025-07-20 10:02:26 +0900

    add exp

--- trainer/downstream/scripts/cross_validation_finetune_7CV_phase2/cross_validation_base.sh
+++ trainer/downstream/scripts/cross_validation_finetune_7CV_phase2/cross_validation_base.sh
@@ -19,6 +19,7 @@ batch_size=32
 freeze_encoder=False
 use_alpha=False  # since "use_weighted_sampler" is ture.
 smoothing=0.01
+LR=1e-4
 
 model_path=/team/team_blu3/lung/project/luna25/weights/nodulex-v5.0.8rc1/cv_fine_model7_val_fold${val_fold}_7CV/model_auroc.pth
 
@@ -44,6 +45,7 @@ HYDRA_FULL_ERROR=1 python3 main.py \
   "loader.dataset.dataset_infos.luna25.fold_key=${fold_key}" \
   loader.batch_size=${batch_size} \
   scheduler=reduce_on_plateau \
+  optim.optim_repr.lr=${LR} \
   trainer.max_epoch=${epoch} \
   trainer.fine_tune_info.enable=True \
   trainer.fine_tune_info.freeze_encoder=${freeze_encoder} \
