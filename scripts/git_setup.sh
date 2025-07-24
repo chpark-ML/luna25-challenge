ff7d7bda5562a3a6d274cdc7551c6855392260ad
chpark <bch0322@gmail.com>
2025-07-24 14:22:47 +0900

    update git setup

--- scripts/git_setup.sh
+++ scripts/git_setup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# activate git credential store
-git config --global --unset credential.helper
-git config --global credential.helper store
+# unset git credential store
+# git config --global --unset credential.helper
+# git config --global --unset user.name
+# git config --global --unset user.email
+
+git config credential.helper store
 
 # git user.name, user.email
 read -p "Git user.name: " username
 read -p "Git user.email: " email
 
 # Set the global Git configuration for user name and email
-git config --global user.name "$username"
-git config --global user.email "$email"
+git config user.name "$username"
+git config user.email "$email"
+
+# Check all settings applied in the current directory
+git config --list --show-origin
+
